31d618d52c96f1af95ece1a1f586b4bdd3b282e3
MatheusMed <dutra.md@hotmail.com>
2021-11-21 17:02:55 -0300

    api funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
-      home: const MovieHome(),
+      home: MovieHome(),
     );
   }
 }
--- lib/ui/movie_home/movie_home.dart
+++ lib/ui/movie_home/movie_home.dart
@@ -1,9 +1,11 @@
 import 'package:app_filmes/controllers/movie_controller.dart';
 import 'package:app_filmes/models/movie_genre.dart';
+import 'package:app_filmes/models/movie_model_popular.dart';
 import 'package:app_filmes/repositories/movie_repository/movie_repository.dart';
 import 'package:app_filmes/repositories/movie_repository/movie_repository_impl.dart';
 import 'package:app_filmes/services/dio_services/dio_services.dart';
 import 'package:app_filmes/services/dio_services/dio_services_impl.dart';
+import 'package:app_filmes/utils/api.utils.dart';
 import 'package:flutter/material.dart';
 
 class MovieHome extends StatefulWidget {
@@ -28,7 +30,39 @@ class _MovieHomeState extends State<MovieHome> {
       appBar: AppBar(
         title: Text('Filmes'),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: ValueListenableBuilder<MovieModelPopular?>(
+                valueListenable: controller.moviesPopular,
+                builder: (_, movies, __) {
+                  return movies != null
+                      ? ListView.separated(
+                          separatorBuilder: (_, __) => SizedBox(height: 20),
+                          shrinkWrap: true,
+                          itemCount: movies.listMovies.length,
+                          itemBuilder: (ctx, idx) {
+                            return Container(
+                              height: 550,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: NetworkImage(ApiUtils.REQUEST_IMG(
+                                    movies.listMovies[idx].posterPath,
+                                  )),
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                      : Container();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
